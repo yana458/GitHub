a4c3f0a18a225ad21e494f2561951523851ef8a2
Daw1 <Daw1@MEDREGAL26>
2025-02-17 16:55:33 +0000

    Version 2.0: Añadimos dos variables enteras a y b e imprimimos su suma

--- GittHubb/src/gitthubb/GittHubb.java
+++ GittHubb/src/gitthubb/GittHubb.java
@@ -12,6 +12,11 @@ public class GittHubb {
 
     public static void main(String[] args) {
         System.out.println("Comenzamos el proyecto de GitHub en Netbeans");
+        System.out.println("Realizamos el primer cambio: ");
+        int a = 6;
+        int b = 7;
+        int suma = a + b;
+        System.out.println("La suma de a y b es: " + suma);
     }
     
 }
