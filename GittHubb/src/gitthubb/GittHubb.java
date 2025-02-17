b909c74d9a778d76d3fac2cc64e594c37f2bd4d4
Daw1 <Daw1@MEDREGAL26>
2025-02-17 16:54:09 +0000

    Version 1: Comenzando el proyecto de GitHUb en Netbeans

---
+++ GittHubb/src/gitthubb/GittHubb.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitthubb;
+
+/**
+ *
+ * @author Yana 1ºDAW
+ */
+public class GittHubb {
+
+    public static void main(String[] args) {
+        System.out.println("Comenzamos el proyecto de GitHub en Netbeans");
+    }
+    
+}
